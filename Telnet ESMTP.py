f0caf02d9670e17ccf4405e3967c08efe10c70a6
drozdovvv <37142791+drozdovvv@users.noreply.github.com>
2019-08-24 03:28:41 +0200

    Create Telnet ESMTP.py

---
+++ Telnet ESMTP.py
@@ -0,0 +1,16 @@
+telnet poczta.interia.pl 576
+EHLO mati
+AUTH LOGIN
+YW5uLnJlZC5mb3g3NUBnbWFpbC5jb20K
+NzVhZm9oaXYKCg==
+
+MAIL FROM: <pas.2018@interia.pl>
+RCPT TO: <mateusz.a.kozlowski@gmail.com>
+DATA
+To: <mateusz.a.kozlowski@gmail.com>
+From: <ann.red.fox75@gmail.com>
+Subject: Hallo
+		
+cos tam
+.
+QUIT
